8a01c340f65cb2203c2c76623125e6eff418f361
ZZJ Cmxxd <cmxxdzzj@qq.com>
2024-04-08 14:43:54 +0800

    24.04.08

---
+++ Chap04/Dog.java
@@ -0,0 +1,5 @@
+package Chap04;
+
+public class Dog extends Animal{
+
+}
---
+++ Chap04/TestZoo.java
@@ -0,0 +1,11 @@
+package Chap04;
+
+public class TestZoo {
+    public static void main(String[] args) {
+        
+        Animal animal[] = new Animal[3];
+        animal[0] = new Dog();
+        animal[1] = new Cat();
+        animal[3] = new Dog();
+    }
+}
