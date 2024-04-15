82f6884fc7824e679d10583d036809abde9949d6
ZZJ Cmxxd <cmxxdzzj@qq.com>
2024-04-15 13:53:36 +0800

    24.04.15

--- Chap04/Animal.java
+++ Chap04/Animal.java
@@ -1,5 +1,15 @@
 package Chap04;
 
-public class Animal {
+abstract public class Animal {
     
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
 }
